7b9734620c68e7198804ccc3c0f2f9a96bab0bf5
mon1t0r <westxlebyshek@gmail.com>
2025-08-16 20:33:58 +0200

    Main program implementation progress

--- src/main.c
+++ src/main.c
@@ -19,10 +19,110 @@ enum {
     input_buf_sz = 256
 };
 
-static pres action_handle(const struct img_ctx *img_ctx,
-                          struct schem_ctx *schem_ctx, int sym)
+enum action_res {
+    action_continue, action_exit_ok, action_exit_fatal
+};
+
+static void schem_print_mbr(const struct schem_ctx_mbr *s_ctx_mbr)
+{
+    const struct mbr *mbr;
+    int i;
+    const struct mbr_part *part;
+    pu32 c, h, s;
+
+    mbr = &s_ctx_mbr->mbr;
+
+    printf("Disk identifier: 0x%08lx\n", mbr->disk_sig);
+
+    printf("\n===Partitions===\n\n");
+
+    for(i = 0; i < ARRAY_SIZE(mbr->partitions); i++) {
+        part = &mbr->partitions[i];
+
+        /* Empty partition */
+        if(!mbr_is_part_used(part)) {
+            break;
+        }
+
+        printf("=Partition №%d\n", i + 1);
+        printf("Boot         %u\n", part->boot_ind);
+        printf("Type         %u\n", part->type);
+        printf("Start LBA    %lu\n", part->start_lba);
+        printf("End LBA      %lu\n", part->start_lba + part->sz_lba - 1);
+        printf("Sectors      %lu\n", part->sz_lba);
+
+        chs_int_to_tuple(part->start_chs, &c, &h, &s);
+        printf("Start C/H/S  %03lu/%03lu/%03lu\n", c, h, s);
+
+        chs_int_to_tuple(part->end_chs, &c, &h, &s);
+        printf("End C/H/S    %03lu/%03lu/%03lu\n\n", c, h, s);
+    }
+}
+
+static void schem_print_gpt(const struct schem_ctx_gpt *s_ctx_gpt)
+{
+    const struct gpt_hdr *hdr;
+    const struct gpt_part_ent *part;
+    char buf[50];
+    int i;
+
+    hdr = &s_ctx_gpt->hdr_prim;
+
+    guid_to_str(buf, &hdr->disk_guid);
+    printf("Disk identifier: %s\n", buf);
+
+    printf("\n===Partitions===\n\n");
+
+    for(i = 0; i < hdr->part_table_entry_cnt; i++) {
+        part = &s_ctx_gpt->table_prim[i];
+
+        /* Empty partition */
+        if(!gpt_is_part_used(part)) {
+            break;
+        }
+
+        guid_to_str(buf, &part->unique_guid);
+        printf("=Partition %s\n", buf);
+
+        guid_to_str(buf, &part->type_guid);
+        printf("Type        %s\n", buf);
+
+        printf("Start LBA   %llu\n", part->start_lba);
+        printf("End LBA     %llu\n", part->end_lba);
+        printf("Sectors     %llu\n", part->end_lba - part->start_lba + 1);
+
+        /* TODO: Print partition name */
+    }
+}
+
+static void schem_print(const struct schem_ctx *schem_ctx)
+{
+    switch(schem_ctx->type) {
+        case schem_mbr:
+            printf("Partitioning scheme: MBR\n");
+            schem_print_mbr(&schem_ctx->s.s_mbr);
+            break;
+
+        case schem_gpt:
+            printf("Partitioning scheme: GPT\n");
+            schem_print_gpt(&schem_ctx->s.s_gpt);
+            break;
+
+        case schem_none:
+            printf("Partitioning scheme: None\n");
+            break;
+    }
+}
+
+static enum action_res
+action_handle(const struct img_ctx *img_ctx, struct schem_ctx *schem_ctx,
+              int sym)
 {
     switch(sym) {
+        /* Exit */
+        case 'q':
+            return action_exit_ok;
+
         /* Help */
         case 'm':
             printf("*help should be here*\n");
@@ -30,10 +130,16 @@ static pres action_handle(const struct img_ctx *img_ctx,
 
         /* Print the partition table */
         case 'p':
+            schem_print(schem_ctx);
+            break;
+
+        /* Unknown */
+        default:
+            printf("Unknown command\n");
             break;
     }
 
-    return pres_ok;
+    return action_continue;
 }
 
 static pres
@@ -41,24 +147,29 @@ routine_start(const struct img_ctx *img_ctx, struct schem_ctx *schem_ctx)
 {
     char buf[input_buf_sz];
     char *s;
-    pres r;
+    enum action_res res;
 
     for(;;) {
-        printf("Command (m for help): ");
+        printf("\nCommand (m for help): ");
 
         s = fgets(buf, sizeof(buf), stdin);
 
-        printf("\n");
-
-        /* Character + '\n' */
-        if(s == NULL || strlen(s) != 2) {
+        /* End of file */
+        if(s == NULL) {
+            printf("\n");
             return pres_ok;
         }
 
-        r = action_handle(img_ctx, schem_ctx, s[0]);
-        if(!r) {
-            return pres_fail;
+        if(strlen(s) != 2) {
+            continue;
         }
+
+        res = action_handle(img_ctx, schem_ctx, s[0]);
+        if(res == action_continue) {
+            continue;
+        }
+
+        return res == action_exit_ok ? pres_ok : pres_fail;
     }
 }
 
